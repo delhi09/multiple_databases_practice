8c5e3950dfe50fa87c6dc83b57b8eb17ad279800
delhi09 <masataka.okudera.biz@gmail.com>
2020-05-16 06:42:11 +0900

    コミット

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,13 @@
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 
 
 def index(request):
+    user1 = User(username="fizz")
+    user1.set_password("password")
+    user1.save()
+
+    user2 = User(username="buzz")
+    user2.set_password("password")
+    user2.save(using="dbserver2")
     return HttpResponse("Hello, world. You're at the polls index.")
